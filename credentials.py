d4ba8aa4ab69abdfa3838a852ae2ea3bd205a3e4
Gaurav Shukla <55787508+iamgauravshukla@users.noreply.github.com>
2020-05-28 20:46:52 +0530

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,5 +1,5 @@
 #Things we  must need while creating Twilio Sms app
-accountSid = 'ACd6aa8233fae7a33211bd23f2bbc8431a'
-authToken = '486fece3c599ca21dfef0a8355717be8'
+accountSid = 'Enter your twilio account sid here'
+authToken = 'Enter your authorised token here'
 myCell = '+917860838636'
 myTwilio = '+12058831828'
